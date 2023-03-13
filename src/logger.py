6449e33da71354a0c88760f8059f820d0bd7482f
zeeshan <through.learning@gmail.com>
2023-03-13 18:51:13 +0530

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,15 @@ import logging
 import os
 
 from datetime import datetime
+from datetime import date
 
 LOG_FILE = (
     # Create the name of the file
     f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 )
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+
+logs_path = os.path.join(os.getcwd(), 'logs', f"{date.today()}")
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
